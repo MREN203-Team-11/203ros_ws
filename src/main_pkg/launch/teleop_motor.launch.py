eae6946bf7ff4cf564b21e6671c95812999a3381
Liam <liamshan13@gmail.com>
2026-03-29 11:54:47 -0400

    launch file edits

--- src/main_pkg/launch/teleop_motor.launch.py
+++ src/main_pkg/launch/teleop_motor.launch.py
@@ -1,4 +1,6 @@
 from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch_ros.actions import Node
 from ament_index_python.packages import get_package_share_directory
 import os
@@ -11,7 +13,19 @@ def generate_launch_description():
     teleop_params = os.path.join(pkg_share, 'config', 'teleop_joy.yaml')
     odom_params = os.path.join(pkg_share, 'config', 'odometry_parameters.yaml')
 
+    rsp = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource([
+            os.path.join(pkg_share, 'launch', 'rsp.launch.py')
+        ]),
+        launch_arguments={
+            'use_sim_time': 'false',
+            'use_ros2_control': 'true',
+        }.items()
+    )
+
     return LaunchDescription([
+        rsp,
+
         Node(
             package='joy',
             executable='joy_node',
